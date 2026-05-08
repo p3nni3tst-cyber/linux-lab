f9ec17e2f9f770c5b24f1e6a9e9f9cf4d348f690
p3nni3tst-cyber <p3nni3tst@gmail.com>
2026-05-08 17:33:44 -0400

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+git clone https://github.com/p3nni3tst-cyber/linux-lab.git
+cd linux-lab
+echo "Hello from Linux lab" > hello.txt
+git add hello.txt
+git commit -m "First commit"
+git push origin main
